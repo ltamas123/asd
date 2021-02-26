e0abf6050c2c30ec78f012991fef0299b8408d4f
Lázár Tamás <ltamas420@gmail.com>
2021-02-26 15:06:58 +0100

    add existing project

---
+++ main.js
@@ -0,0 +1,15 @@
+let messageArray = [];
+let counter = 0;
+
+const fn = () => {
+  console.log("Enter massage: ");
+  scan = keyscan.make_scanner((ch) => message(ch.parsed));
+};
+
+const message = (char) => {
+  messageArray.push(char.charCodeAt(0));
+
+  //process.stdout.write(messageArray[counter]);
+
+  counter++;
+};
